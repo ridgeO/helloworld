e5c4b8867d50bff429d5690d36d58220108e43a9
Ridge <rto@MBAir.local>
2017-08-18 14:10:26 -0700

    init and publish

---
+++ hello.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+exports class HelloWorld extends Component {
+  render() {
+    return {
+      <Text>Hello World!</Text>
+    }
+  }
+}
